4303dabbbf899ef5cd1f485f2b8be6a60388b579
Wiliam José de Carvalho Jesus <wjesus@daitan.com>
2022-07-28 22:00:25 -0300

    removing serve html

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const db = require("./database")
 const express = require("express")
 const bodyParser = require("body-parser")
-const path = require("path")
 
 const app = express()
 app.use(bodyParser.json());
@@ -10,12 +9,8 @@ app.use(bodyParser.urlencoded({
 }));
 const port = process.env.PORT || 3000
 
-//serve html
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
-});
 app.listen(port,()=>{
-    console.log(`Server running at port ${port}`)
+    console.log(`Server running at port ${port}`) 
 })
 
 app.get("/produtos",(req,res) => {
